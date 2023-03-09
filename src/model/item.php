57371f5c2b77ac06bc919b14af22a490f9cb3249
Suphawat Sujai <suphawat_2544_196@hotmail.com>
2023-03-10 01:28:56 +0700

    Update item.php

--- src/model/item.php
+++ src/model/item.php
@@ -1,3 +1,18 @@
 <?php
 namespace App\model;
+
+use App\Database\contaxt;
+
+class item extends Contaxt{
+
+    public function getAllitem(){
+        $sql ="
+        select * FROM items
+        ";
+
+        $stmt = $this->pdo->query($sql);
+        $data = $stmt->fetchAll();
+        return $data;
+    }
+}
 ?>
